338e3bad0bdc816a53469124d009edbbc9b8a705
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2025-02-20 15:01:23 -0400

    feat(edit-event): mejorar la obtención del ID del evento utilizando promesas

--- src/app/edit-event/[id]/page.tsx
+++ src/app/edit-event/[id]/page.tsx
@@ -2,12 +2,13 @@ import { notFound } from "next/navigation";
 import { EventDetails } from "@/components/event-details";
 import { getEventById } from "@/src/agenda/actions/get-event-by-id";
 
-export default async function EditEventPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const event = await getEventById(params.id);
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+export default async function EditEventPage({ params }: Props) {
+  const { id } = await params;
+  const event = await getEventById(id);
 
   if (!event) {
     notFound();
