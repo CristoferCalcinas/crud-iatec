ff669f4a94ef331771c1025903be2b46f3a22221
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2025-02-20 12:27:30 -0400

    feat(events): mejorar la carga y filtrado de eventos por fecha en la página de edición

--- src/agenda/actions/get-event-by-user.ts
+++ src/agenda/actions/get-event-by-user.ts
@@ -1,27 +1,32 @@
 "use server";
 
 import { auth } from "@/auth";
-
 import { prisma } from "@/prisma";
 
 export const getEventByUser = async () => {
   const session = await auth();
-  if (!session || !session.user || !session.user.id) {
-    console.error("No se pudo obtener la sesión del usuario.");
-    return null;
-  }
 
-  const userId = session.user.id;
+  if (!session?.user?.id) return null;
 
   try {
     return await prisma.event.findMany({
-      where: { userId },
+      where: {
+        userId: session.user.id,
+      },
       include: {
         categories: true,
+        participants: {
+          include: {
+            user: true,
+          },
+        },
+      },
+      orderBy: {
+        startTime: "asc",
       },
     });
   } catch (error) {
-    console.error("Error cargando la agenda:", error);
-    return [];
+    console.error("Error obteniendo eventos:", error);
+    return null;
   }
 };
